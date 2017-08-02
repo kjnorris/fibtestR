699bec58be6ad925d5ae8f5427b1a0823a33b915
Kenneth Norris <kjnorris@gmail.com>
2017-08-02 08:24:53 -0400

    Fixed unit test call - expect_equal

--- tests/testthat/testLowFibs.R
+++ tests/testthat/testLowFibs.R
@@ -7,7 +7,7 @@ test_that("Defined Fibonacci numbers are 1", {
 })
 
 test_that("Random low Fibonacci numbers", {
-  expect_that(getFib(3), 2)
-  expect_that(getFib(5), 5)
-  expect_that(getFib(9), 34)
+  expect_equal(getFib(3), 2)
+  expect_equal(getFib(5), 5)
+  expect_equal(getFib(9), 34)
 })
